8e5bf7e727bde5ec143163c40311ff6d8918d719
annia valdes <millerl.mb79@gmail.com>
2020-06-12 16:56:09 -0500

    style end

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+ 
+  var highScore=100;
+  var time=100;
+  var index=0;
+  var buttonD=document.querySelector("#quiz");
+
+
+var question =[
+    {
+        title:"",
+        res:["r1","r2","r3"],
+        correct:0  //posisicion
+},
+]
+
+function showQuestions(){
+
+    var question1=document.createElement("h2");   
+    question1.textContent=" Who created JavaScript?";
+
+    // show the question
+    //show the response
+    // add the event lestener
+    // when on click the veriy, increase the index verify if you have mores questions if yes go to showestio  if not go to results
+
+}
+
+
+
+function starTimer(){
+
+
+
+}
+
+
+function results(){
+
+
+
+}
+
+buttonD.addEventListener("#quiz",showQuestions());
+  // add event listern to the #quiz
+  // clear the area 
+  // showQuestions()
+  // startTimer()
